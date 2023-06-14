37b3d441fdcedba037dd502527fc8923adb644ab
Alciviades Cueva <acuevae9@gmail.com>
2023-06-14 09:56:53 -0500

    footer

--- vistas/modulos/aplicativo-movil.php
+++ vistas/modulos/aplicativo-movil.php
@@ -1,10 +1,10 @@
 <div class="container-fluid bg-primary py-5 bg-header" style="margin-bottom: 90px;">
             <div class="row py-5">
                 <div class="col-12 pt-lg-5 mt-lg-5 text-center">
-                    <h1 class="display-4 text-white animated zoomIn">Services</h1>
-                    <a href="" class="h5 text-white">Home</a>
+                    <h1 class="display-4 text-white animated zoomIn">Servicios</h1>
+                    <a href="" class="h5 text-white">Inicio</a>
                     <i class="far fa-circle text-white px-2"></i>
-                    <a href="" class="h5 text-white">Services</a>
+                    <a href="" class="h5 text-white">Aplicativos</a>
                 </div>
             </div>
         </div>
@@ -35,8 +35,8 @@
     <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
         <div class="container py-5">
             <div class="section-title text-center position-relative pb-3 mb-5 mx-auto" style="max-width: 600px;">
-                <h5 class="fw-bold text-primary text-uppercase">Our Services</h5>
-                <h1 class="mb-0">Custom IT Solutions for Your Successful Business</h1>
+                <h5 class="fw-bold text-primary text-uppercase">Nuestros servicios</h5>
+                <h1 class="mb-0">Desarrollo de Aplicativos móviles</h1>
             </div>
             <div class="row g-5">
                 <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.3s">
