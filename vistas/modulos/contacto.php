00b555e91cfc847701f5c71343372b46543b108d
Jhosmer <jguevaram0897@gmail.com>
2023-06-14 12:13:03 -0500

    mapa-contacto

--- vistas/modulos/contacto.php
+++ vistas/modulos/contacto.php
@@ -1,10 +1,10 @@
 <div class="container-fluid bg-primary py-5 bg-header" style="margin-bottom: 90px;">
     <div class="row py-5">
         <div class="col-12 pt-lg-5 mt-lg-5 text-center">
-            <h1 class="display-4 text-white animated zoomIn">Contact Us</h1>
-            <a href="" class="h5 text-white">Home</a>
+            <h1 class="display-4 text-white animated zoomIn">Contáctanos</h1>
+            <!-- <a href="" class="h5 text-white">Inicio</a>
             <i class="far fa-circle text-white px-2"></i>
-            <a href="" class="h5 text-white">Contact</a>
+            <a href="" class="h5 text-white">Contacto</a> -->
         </div>
     </div>
 </div>
@@ -35,8 +35,8 @@
 <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
     <div class="container py-5">
         <div class="section-title text-center position-relative pb-3 mb-5 mx-auto" style="max-width: 600px;">
-            <h5 class="fw-bold text-primary text-uppercase">Contact Us</h5>
-            <h1 class="mb-0">If You Have Any Query, Feel Free To Contact Us</h1>
+            <h5 class="fw-bold text-primary text-uppercase">Contáctanos</h5>
+            <h1 class="mb-0">¿Necesita una cotización gratis? No dude en ponerse en contacto con nosotros</h1>
         </div>
         <div class="row g-5 mb-5">
             <div class="col-lg-4">
@@ -45,8 +45,8 @@
                         <i class="fa fa-phone-alt text-white"></i>
                     </div>
                     <div class="ps-4">
-                        <h5 class="mb-2">Call to ask any question</h5>
-                        <h4 class="text-primary mb-0">+012 345 6789</h4>
+                        <h5 class="mb-2">Contáctate con nostros</h5>
+                        <h6 class="text-primary mb-0">+51 997 226 188</h6>
                     </div>
                 </div>
             </div>
@@ -56,8 +56,8 @@
                         <i class="fa fa-envelope-open text-white"></i>
                     </div>
                     <div class="ps-4">
-                        <h5 class="mb-2">Email to get free quote</h5>
-                        <h4 class="text-primary mb-0">info@example.com</h4>
+                        <h5 class="mb-2">Correo electrónico para consultas</h5>
+                        <h6 class="text-primary mb-0">techologies@geotekhperu.com</h6>
                     </div>
                 </div>
             </div>
@@ -67,8 +67,8 @@
                         <i class="fa fa-map-marker-alt text-white"></i>
                     </div>
                     <div class="ps-4">
-                        <h5 class="mb-2">Visit our office</h5>
-                        <h4 class="text-primary mb-0">123 Street, NY, USA</h4>
+                        <h5 class="mb-2">Visita nuestra oficina</h5>
+                        <h6 class="text-primary mb-0">Clodomiro Cerna #360, Cajamarca, Perú</h6>
                     </div>
                 </div>
             </div>
@@ -78,25 +78,25 @@
                 <form>
                     <div class="row g-3">
                         <div class="col-md-6">
-                            <input type="text" class="form-control border-0 bg-light px-4" placeholder="Your Name" style="height: 55px;">
+                            <input type="text" class="form-control border-0 bg-light px-4" placeholder="Tu nombre" style="height: 55px;">
                         </div>
                         <div class="col-md-6">
-                            <input type="email" class="form-control border-0 bg-light px-4" placeholder="Your Email" style="height: 55px;">
+                            <input type="email" class="form-control border-0 bg-light px-4" placeholder="Tu correo" style="height: 55px;">
                         </div>
                         <div class="col-12">
-                            <input type="text" class="form-control border-0 bg-light px-4" placeholder="Subject" style="height: 55px;">
+                            <input type="text" class="form-control border-0 bg-light px-4" placeholder="Asunto" style="height: 55px;">
                         </div>
                         <div class="col-12">
-                            <textarea class="form-control border-0 bg-light px-4 py-3" rows="4" placeholder="Message"></textarea>
+                            <textarea class="form-control border-0 bg-light px-4 py-3" rows="4" placeholder="Mensaje"></textarea>
                         </div>
                         <div class="col-12">
-                            <button class="btn btn-primary w-100 py-3" type="submit">Send Message</button>
+                            <button class="btn btn-primary w-100 py-3" type="submit">ENVIAR</button>
                         </div>
                     </div>
                 </form>
             </div>
             <div class="col-lg-6 wow slideInUp" data-wow-delay="0.6s">
-                <iframe class="position-relative rounded w-100 h-100" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sbd!4v1603794290143!5m2!1sen!2sbd" frameborder="0" style="min-height: 350px; border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                <iframe class="position-relative rounded w-100 h-100" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15834.711642878076!2d-78.4989736!3d-7.163184!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91b25bf905298bd1%3A0x2256d97aea37b1f7!2sGEOTEKH%20PER%C3%9A!5e0!3m2!1ses!2spe!4v1686762641334!5m2!1ses!2spe" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" frameborder="0" style="min-height: 350px; border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
             </div>
         </div>
     </div>
--- vistas/modulos/nosotros.php
+++ vistas/modulos/nosotros.php
@@ -1,10 +1,10 @@
 <div class="container-fluid bg-primary py-5 bg-header" style="margin-bottom: 90px;">
             <div class="row py-5">
                 <div class="col-12 pt-lg-5 mt-lg-5 text-center">
-                    <h1 class="display-4 text-white animated zoomIn">About Us</h1>
-                    <a href="" class="h5 text-white">Home</a>
+                    <h1 class="display-4 text-white animated zoomIn">Acerca de nosotros</h1>
+                    <a href="" class="h5 text-white">Inicio</a>
                     <i class="far fa-circle text-white px-2"></i>
-                    <a href="" class="h5 text-white">About</a>
+                    <a href="" class="h5 text-white">Acerca</a>
                 </div>
             </div>
         </div>
