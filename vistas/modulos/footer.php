9a45f1aced06783c87dc02d0217bc4cc488ac63a
Ronal Ramos <ronalirv23@gmail.com>
2023-06-14 11:34:06 -0500

    cambios

--- vistas/modulos/footer.php
+++ vistas/modulos/footer.php
@@ -84,4 +84,4 @@
 
 
     <!-- Back to Top -->
-    <a href="#" class="btn btn-lg btn-primary btn-lg-square rounded back-to-top"><i class="bi bi-arrow-up"></i></a>
+    <a href="#ini" class="btn btn-lg btn-primary btn-lg-square rounded back-to-top"><i class="bi bi-arrow-up"></i></a>
