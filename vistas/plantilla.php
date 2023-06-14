8d1bbccd787eb953b322da7cc81aeb81979104fc
Alciviades Cueva <acuevae9@gmail.com>
2023-06-14 11:32:59 -0500

    logos

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -4,7 +4,7 @@
 <head>
     <!-- Links -->
     <meta charset="utf-8">
-    <title>technologies - tcs</title>
+    <title>Geotekh Technologies</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="Free HTML Templates" name="keywords">
     <meta content="Free HTML Templates" name="description">
