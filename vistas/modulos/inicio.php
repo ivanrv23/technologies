570904ff3b76346e4c623ba19f4f8a1dfd1d24ab
Jhosmer <jguevaram0897@gmail.com>
2023-06-15 11:57:31 -0500

    imagenes

--- vistas/modulos/inicio.php
+++ vistas/modulos/inicio.php
@@ -61,7 +61,7 @@
         <div class="container py-5 pt-lg-0">
             <div class="row gx-0">
                 <div class="col-lg-4 wow zoomIn" data-wow-delay="0.1s">
-                    <div class="bg-primary shadow d-flex align-items-center justify-content-center p-4" style="height: 150px;">
+                    <div class="bg-primary shadow d-flex align-items-center justify-content-center p-4" style="height: 170px;">
                         <div class="bg-white d-flex align-items-center justify-content-center rounded mb-2" style="width: 60px; height: 60px;">
                             <i class="fa fa-users text-primary"></i>
                         </div>
@@ -72,7 +72,7 @@
                     </div>
                 </div>
                 <div class="col-lg-4 wow zoomIn" data-wow-delay="0.3s">
-                    <div class="bg-light shadow d-flex align-items-center justify-content-center p-4" style="height: 150px;">
+                    <div class="bg-light shadow d-flex align-items-center justify-content-center p-4" style="height: 170px;">
                         <div class="bg-primary d-flex align-items-center justify-content-center rounded mb-2" style="width: 60px; height: 60px;">
                             <i class="fa fa-check text-white"></i>
                         </div>
@@ -83,7 +83,7 @@
                     </div>
                 </div>
                 <div class="col-lg-4 wow zoomIn" data-wow-delay="0.6s">
-                    <div class="bg-primary shadow d-flex align-items-center justify-content-center p-4" style="height: 150px;">
+                    <div class="bg-primary shadow d-flex align-items-center justify-content-center p-4" style="height: 170px;">
                         <div class="bg-white d-flex align-items-center justify-content-center rounded mb-2" style="width: 60px; height: 60px;">
                             <i class="fa fa-award text-primary"></i>
                         </div>
@@ -557,15 +557,15 @@
     <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
         <div class="container py-5">
             <div class="section-title text-center position-relative pb-3 mb-5 mx-auto" style="max-width: 600px;">
-                <h5 class="fw-bold text-primary text-uppercase">Latest Blog</h5>
-                <h1 class="mb-0">Read The Latest Articles from Our Blog Post</h1>
+                <!-- <h5 class="fw-bold text-primary text-uppercase">NUESTROS PROYECTOS</h5> -->
+                <h1 class="mb-0">NUESTROS PROYECTOS</h1>
             </div>
             <div class="row g-5">
                 <div class="col-lg-4 wow slideInUp" data-wow-delay="0.3s">
                     <div class="blog-item bg-light rounded overflow-hidden">
                         <div class="blog-img position-relative overflow-hidden">
-                            <img class="img-fluid" src="vistas/img/blog-1.jpg" alt="">
-                            <a class="position-absolute top-0 start-0 bg-primary text-white rounded-end mt-5 py-2 px-4" href="">Web Design</a>
+                            <img class="img-fluid" src="vistas/img/maintek-mockup.png" alt="">
+                            <a class="position-absolute top-0 start-0 bg-primary text-white rounded-end mt-5 py-2 px-4" href="">MAINTEKH</a>
                         </div>
                         <div class="p-4">
                             <div class="d-flex mb-3">
@@ -581,8 +581,8 @@
                 <div class="col-lg-4 wow slideInUp" data-wow-delay="0.6s">
                     <div class="blog-item bg-light rounded overflow-hidden">
                         <div class="blog-img position-relative overflow-hidden">
-                            <img class="img-fluid" src="vistas/img/blog-2.jpg" alt="">
-                            <a class="position-absolute top-0 start-0 bg-primary text-white rounded-end mt-5 py-2 px-4" href="">Web Design</a>
+                            <img class="img-fluid" src="vistas/img/logtekh-mockup.png" alt="">
+                            <a class="position-absolute top-0 start-0 bg-primary text-white rounded-end mt-5 py-2 px-4" href="">LOGTEKH</a>
                         </div>
                         <div class="p-4">
                             <div class="d-flex mb-3">
@@ -599,7 +599,58 @@
                     <div class="blog-item bg-light rounded overflow-hidden">
                         <div class="blog-img position-relative overflow-hidden">
                             <img class="img-fluid" src="vistas/img/blog-3.jpg" alt="">
-                            <a class="position-absolute top-0 start-0 bg-primary text-white rounded-end mt-5 py-2 px-4" href="">Web Design</a>
+                            <a class="position-absolute top-0 start-0 bg-primary text-white rounded-end mt-5 py-2 px-4" href="">GEOMEKH</a>
+                        </div>
+                        <div class="p-4">
+                            <div class="d-flex mb-3">
+                                <small class="me-3"><i class="far fa-user text-primary me-2"></i>John Doe</small>
+                                <small><i class="far fa-calendar-alt text-primary me-2"></i>01 Jan, 2045</small>
+                            </div>
+                            <h4 class="mb-3">How to build a website</h4>
+                            <p>Dolor et eos labore stet justo sed est sed sed sed dolor stet amet</p>
+                            <a class="text-uppercase" href="">Read More <i class="bi bi-arrow-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 wow slideInUp" data-wow-delay="0.9s">
+                    <div class="blog-item bg-light rounded overflow-hidden">
+                        <div class="blog-img position-relative overflow-hidden">
+                            <img class="img-fluid" src="vistas/img/blog-3.jpg" alt="">
+                            <a class="position-absolute top-0 start-0 bg-primary text-white rounded-end mt-5 py-2 px-4" href="">MINERAL transparent</a>
+                        </div>
+                        <div class="p-4">
+                            <div class="d-flex mb-3">
+                                <small class="me-3"><i class="far fa-user text-primary me-2"></i>John Doe</small>
+                                <small><i class="far fa-calendar-alt text-primary me-2"></i>01 Jan, 2045</small>
+                            </div>
+                            <h4 class="mb-3">How to build a website</h4>
+                            <p>Dolor et eos labore stet justo sed est sed sed sed dolor stet amet</p>
+                            <a class="text-uppercase" href="">Read More <i class="bi bi-arrow-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 wow slideInUp" data-wow-delay="0.9s">
+                    <div class="blog-item bg-light rounded overflow-hidden">
+                        <div class="blog-img position-relative overflow-hidden">
+                            <img class="img-fluid" src="vistas/img/blog-3.jpg" alt="">
+                            <a class="position-absolute top-0 start-0 bg-primary text-white rounded-end mt-5 py-2 px-4" href="">SISTEMA TRUJILLO</a>
+                        </div>
+                        <div class="p-4">
+                            <div class="d-flex mb-3">
+                                <small class="me-3"><i class="far fa-user text-primary me-2"></i>John Doe</small>
+                                <small><i class="far fa-calendar-alt text-primary me-2"></i>01 Jan, 2045</small>
+                            </div>
+                            <h4 class="mb-3">How to build a website</h4>
+                            <p>Dolor et eos labore stet justo sed est sed sed sed dolor stet amet</p>
+                            <a class="text-uppercase" href="">Read More <i class="bi bi-arrow-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 wow slideInUp" data-wow-delay="0.9s">
+                    <div class="blog-item bg-light rounded overflow-hidden">
+                        <div class="blog-img position-relative overflow-hidden">
+                            <img class="img-fluid" src="vistas/img/blog-3.jpg" alt="">
+                            <a class="position-absolute top-0 start-0 bg-primary text-white rounded-end mt-5 py-2 px-4" href="">SISVENT</a>
                         </div>
                         <div class="p-4">
                             <div class="d-flex mb-3">
@@ -618,20 +669,33 @@
     <!-- Blog Start -->
 
 
-    <!-- Vendor Start -->
+
+
     <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
-        <div class="container py-5 mb-5">
+        <div class="container ">
+            <div class="section-title text-center position-relative pb-3 mb-5 mx-auto" style="max-width: 600px;">
+                <h1 class="mb-0">Nuestros clientes</h1>
+            </div>
+            <div class="row g-5">
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Vendor Start -->
+    <div class="container-fluid  wow fadeInUp" data-wow-delay="0.1s">
+        <div class="container mb-5">
             <div class="bg-white">
                 <div class="owl-carousel vendor-carousel">
-                    <img src="vistas/img/academy.png" alt="Cliente">
-                    <img src="vistas/img/alliance.png" alt="Cliente">
-                    <img src="vistas/img/celcom.jpg" alt="Cliente">
-                    <img src="vistas/img/incuba.jpg" alt="Cliente">
-                    <img src="vistas/img/inicia.jpg" alt="Cliente">
-                    <img src="vistas/img/roycell.jpg" alt="Cliente">
-                    <img src="vistas/img/trainers.png" alt="Cliente">
-                    <img src="vistas/img/vendor-8.jpg" alt="Cliente">
-                    <img src="vistas/img/vendor-9.jpg" alt="Cliente">
+                    <img src="vistas/img/creditos-1.png" alt="Cliente" style="width: 170px; height: 100px;" >
+                    <img src="vistas/img/creditos-2.png" alt="Cliente" style="width: 170px; height: 100px;" >
+                    <img src="vistas/img/creditos-3.png" alt="Cliente" style="width: 170px; height: 100px;" >
+                    <img src="vistas/img/creditos-4.png" alt="Cliente" style="width: 170px; height: 100px;" >
+                    <img src="vistas/img/creditos-5.png" alt="Cliente" style="width: 170px; height: 100px;" >
+                    <img src="vistas/img/creditos-6.png" alt="Cliente" style="width: 170px; height: 100px;" >
+                    <img src="vistas/img/creditos-7.png" alt="Cliente" style="width: 170px; height: 100px;" >
+                    <img src="vistas/img/creditos-8.png" alt="Cliente" style="width: 170px; height: 100px;" >
+                    <img src="vistas/img/creditos-9.png" alt="Cliente" style="width: 170px; height: 100px;" >
                 </div>
             </div>
         </div>
