852a6e090efc79e220d41de80d2b575d5798815e
Ronal Ramos <ronalirv23@gmail.com>
2023-06-15 12:09:44 -0500

    inicio

--- vistas/js/funciones.js
+++ vistas/js/funciones.js
@@ -1,12 +1,15 @@
 var url = window.location.pathname;
 var partes = url.split("/");
 var pagina = partes[2];
-console.log(pagina);
 const elementosEnlaces = document.querySelectorAll("#navbarCollapse a");
 elementosEnlaces.forEach((enlace) => {
   const href = enlace.pathname;
   if (window.location.pathname == href) {
     var elemento = document.getElementById(pagina);
+    elemento.classList.add("active");
+  }
+  if (window.location.pathname == "/technologies/") {
+    var elemento = document.getElementById("inicio");
     elemento.classList.add("active");
   }
 });
