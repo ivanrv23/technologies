a4e89d9d0edd7e8b429ff9a684b5222d0de92c3f
Ronal Ramos <ronalirv23@gmail.com>
2023-06-26 11:39:46 -0500

    app

--- vistas/modulos/aplicativo-movil.php
+++ vistas/modulos/aplicativo-movil.php
@@ -2,7 +2,6 @@
     <div class="row py-5">
         <div class="col-12 pt-lg-5 mt-lg-5 text-center">
             <h1 class="display-1 text-white animated zoomIn">APLICATIVOS</h1>
-            <a class="text-light inline" href=""><ins>Brochure</ins></a>
         </div>
     </div>
 </div>
@@ -13,23 +12,24 @@
     <div class="container">
         <div class="row g-5">
             <div class="col-lg-7">
-                <div class="section-title position-relative pb-3 mb-5">
-                    <h5 class="fw-bold text-primary text-uppercase">Sobre nosotros</h5>
-                    <h1 class="mb-0">Contamos con mas de 10 años ofreciendo los mejores servicios tecnológicos para empresas e instituciones</h1>
+                <div class="section-title position-relative pb-2 mb-2">
+                    <h5 class="fw-bold text-primary text-uppercase">DESARROLLO DE APLICACIONES MÓVILES</h5>
                 </div>
-                <p class="mb-4">Somos un equipo de especialistas en soluciones empresariales de tecnología, brindando confianza, compromiso y calidad en cada uno de nuestros servicios, garantizando resultados eficientes.</p>
+                <p style="text-align: justify;">El equipo de Geotekh te ofrece las mejores tecnologías
+                    para el desarrollo de aplicaciones móviles a medida, seguras y escalables.
+                    Diseñamos la mejor experiencia de usuario que aporte
+                    valor a tus clientes e incremente la interacción con tu
+                    marca o negocio en cada uno de tus canales digitales,
+                    potenciando tu marca en las plataformas de Android e
+                    IOS.</p>
                 <div class="row g-0 mb-3">
-                    <div class="col-sm-6 wow zoomIn" data-wow-delay="0.2s">
-                        <h5 class="mb-3"><i class="fa fa-check text-primary me-3"></i>Orientación al cliente</h5>
-                        <h5 class="mb-3"><i class="fa fa-check text-primary me-3"></i>Innovación constante</h5>
-                        <h5 class="mb-3"><i class="fa fa-check text-primary me-3"></i>Competitividad</h5>
-                        <h5 class="mb-3"><i class="fa fa-check text-primary me-3"></i>Equipo de profesionales</h5>
-                    </div>
-                    <div class="col-sm-6 wow zoomIn" data-wow-delay="0.4s">
-                        <h5 class="mb-3"><i class="fa fa-check text-primary me-3"></i>Seguridad</h5>
-                        <h5 class="mb-3"><i class="fa fa-check text-primary me-3"></i>Transparencia</h5>
-                        <h5 class="mb-3"><i class="fa fa-check text-primary me-3"></i>Soporte 24/7</h5>
-                        <h5 class="mb-3"><i class="fa fa-check text-primary me-3"></i>Precios accesibles</h5>
+                    <div class="col-sm-12 wow zoomIn" data-wow-delay="0.2s">
+                        <h5 class="mb-3"><i class="fa fa-check text-primary me-3"></i>Monitorización y gestión de procesos.</h5>
+                        <h5 class="mb-3"><i class="fa fa-check text-primary me-3"></i>Monetización adaptada al modelo de negocio.</h5>
+                        <h5 class="mb-3"><i class="fa fa-check text-primary me-3"></i>Aumento en ventas y reducción de costos</h5>
+                        <h5 class="mb-3"><i class="fa fa-check text-primary me-3"></i>Fidelización de clientes.</h5>
+                        <h5 class="mb-3"><i class="fa fa-check text-primary me-3"></i>Posicionamiento y visibilidad en el mercado.</h5>
+                        <h5 class="mb-3"><i class="fa fa-check text-primary me-3"></i>Reactivación y reducción de la tasa de abandono de clientes.</h5>
                     </div>
                 </div>
                 <div class="d-flex align-items-center mb-4 wow fadeIn" data-wow-delay="0.6s">
@@ -41,7 +41,9 @@
                         <h4 class="text-primary mb-0">+51 997 226 188</h4>
                     </div>
                 </div>
-                <a href="#sc" class="btn btn-primary py-3 px-5 mt-3 wow zoomIn" data-wow-delay="0.9s">Solicitar cotización</a>
+                <a href="contacto" class="btn btn-primary py-3 px-5 mt-3 wow zoomIn" data-wow-delay="0.9s">Solicitar cotización</a>
+                <a href="" class="btn btn-primary py-3 px-5 mt-3 wow zoomIn" data-wow-delay="0.9s">Brochure</a>
+
             </div>
             <div class="col-lg-5" style="min-height: 500px;">
                 <div class="position-relative h-100">
@@ -67,9 +69,7 @@
                     </div>
                     <h4 class="mb-3">Tienda virtual</h4>
                     <p class="m-0">Tu tienda virtual desde tu dispositvo móvil, vende desde cualquier lado.</p>
-                    <a class="btn btn-lg btn-primary rounded" href="">
-                        <i class="bi bi-arrow-right"></i>
-                    </a>
+
                 </div>
             </div>
             <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.6s">
@@ -79,9 +79,6 @@
                     </div>
                     <h4 class="mb-3">Educación</h4>
                     <p class="m-0">Creamos aplicativos para educación.</p>
-                    <a class="btn btn-lg btn-primary rounded" href="">
-                        <i class="bi bi-arrow-right"></i>
-                    </a>
                 </div>
             </div>
             <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.9s">
@@ -91,9 +88,6 @@
                     </div>
                     <h4 class="mb-3">Videojuegos</h4>
                     <p class="m-0">¿Tienes un juego en mente? Hazlo realidad con nuestro equipo profesional de desarrollo.</p>
-                    <a class="btn btn-lg btn-primary rounded" href="">
-                        <i class="bi bi-arrow-right"></i>
-                    </a>
                 </div>
             </div>
             <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.3s">
@@ -103,9 +97,6 @@
                     </div>
                     <h4 class="mb-3">Play Store</h4>
                     <p class="m-0">Te asesoramos para que puedas subir tus aplicativos a Play Store para android.</p>
-                    <a class="btn btn-lg btn-primary rounded" href="">
-                        <i class="bi bi-arrow-right"></i>
-                    </a>
                 </div>
             </div>
             <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.6s">
@@ -115,19 +106,17 @@
                     </div>
                     <h4 class="mb-3">App Store</h4>
                     <p class="m-0">Te asesoramos para que subas tus aplicativos a la App Store de Apple.</p>
-                    <a class="btn btn-lg btn-primary rounded" href="">
-                        <i class="bi bi-arrow-right"></i>
-                    </a>
                 </div>
             </div>
             <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.9s">
-                <div class="position-relative bg-primary rounded h-100 d-flex flex-column align-items-center justify-content-center text-center p-5">
-                    <h3 class="text-white mb-3">Contáctate con nosotros</h3>
-                    <p class="text-white mb-3">Escríbenos sin ningún compromiso al WhatsApp o por nuestras redes sociales</p>
-                    <h2 class="text-white mb-0">+51 997 226 188</h2>
-                </div>
-            </div>
+                <a href="https://api.whatsapp.com/send?phone=997226188&text=Buen%20d%C3%ADa,%20quiero%20m%C3%A1s%20informaci%C3%B3n" target="_blank">
+                <div class=" position-relative bg-primary rounded h-100 d-flex flex-column align-items-center justify-content-center text-center p-5">
+                    <h4 class="text-white mb-3">Contáctate con nosotros</h3>
+                        <p class="text-white mb-3">Escríbenos sin ningún compromiso al WhatsApp o por nuestras redes sociales</p>
+                        <h5 class="text-white mb-0">+51 997 226 188</h2>
+            </div></a>
         </div>
     </div>
+</div>
 </div>
 <!-- Service End -->
