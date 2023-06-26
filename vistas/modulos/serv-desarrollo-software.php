d4703c303c4ef64303aef766a128bd145696da2f
Jhosmer <jguevaram0897@gmail.com>
2023-06-26 11:39:27 -0500

    actualizacion

--- vistas/modulos/serv-desarrollo-software.php
+++ vistas/modulos/serv-desarrollo-software.php
@@ -1,7 +1,7 @@
-<div class="container-fluid bg-primary py-5 bg-header" style="margin-bottom: 90px;">
+<div class="container-fluid bg-primary py-5 bg-header-desarrollo-software" style="margin-bottom: 90px;">
             <div class="row py-5">
                 <div class="col-12 pt-lg-5 mt-lg-5 text-center">
-                    <h1 class="display-1 text-white animated zoomIn">SOFTWARE</h1>
+                    <h1 class="display-1 text-white animated zoomIn">Desarrollo de Software</h1>
                 </div>
             </div>
         </div>
