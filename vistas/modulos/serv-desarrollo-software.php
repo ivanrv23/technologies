d83b7c87d1ac933a913c779a4d0f9b9054ada0b4
Ronal Ramos <ronalirv23@gmail.com>
2023-06-27 11:18:43 -0500

    cambios

--- vistas/modulos/serv-desarrollo-software.php
+++ vistas/modulos/serv-desarrollo-software.php
@@ -1,113 +1,133 @@
 <div class="container-fluid bg-primary py-5 bg-header-desarrollo-software" style="margin-bottom: 90px;">
-            <div class="row py-5">
-                <div class="col-12 pt-lg-5 mt-lg-5 text-center">
-                    <h1 class="display-1 text-white animated zoomIn">Desarrollo de Software</h1>
+    <div class="row py-5">
+        <div class="col-12 pt-lg-5 mt-lg-5 text-center">
+            <h1 class="display-1 text-white animated zoomIn">Desarrollo de Software</h1>
+        </div>
+    </div>
+</div>
+</div>
+<!-- Navbar End -->
+
+<!-- Información Start -->
+<div class="container-fluid wow fadeInUp" data-wow-delay="0.1s">
+    <div class="container">
+        <div class="row g-5">
+            <div class="col-lg-7">
+                <div class="section-title position-relative pb-2 mb-2">
+                    <h5 class="fw-bold text-primary text-uppercase">DESARROLLO DE SOFTWARE</h5>
+                </div>
+                <p style="text-align: justify;">Creamos aplicativos profesionales desarrollados a medida para tu empresa. Nuestro objetivo es optimizar tus procesos y aplicar las últimas tecnologías, manteniéndonos siempre actualizados con el mercado actual. Nos enfocamos en brindar a nuestros clientes productos de excelencia, confiables y seguros, garantizando la confidencialidad, integridad y seguridad de sus datos.</p>
+                <div class="row g-0 mb-3">
+                    <div class="col-sm-12 wow zoomIn" data-wow-delay="0.2s">
+                        <h5 class="mb-3"><i class="fa fa-check text-primary me-3"></i>Mayor eficiencia, agilidad y rendimiento en tus operaciones.</h5>
+                        <h5 class="mb-3"><i class="fa fa-check text-primary me-3"></i>Automatización de procesos.</h5>
+                        <h5 class="mb-3"><i class="fa fa-check text-primary me-3"></i>Mejor toma de desiciones.</h5>
+                        <h5 class="mb-3"><i class="fa fa-check text-primary me-3"></i>Ventaja competitiva.</h5>
+                    </div>
+                </div>
+                <div class="d-flex align-items-center mb-4 wow fadeIn" data-wow-delay="0.6s">
+                    <div class="bg-primary d-flex align-items-center justify-content-center rounded" style="width: 60px; height: 60px;">
+                        <i class="fa fa-phone-alt text-white"></i>
+                    </div>
+                    <div class="ps-4">
+                        <h5 class="mb-2">Para dudas y consultas, comunicarse al número</h5>
+                        <h4 class="text-primary mb-0">+51 997 226 188</h4>
+                    </div>
+                </div>
+                <a href="contacto" class="btn btn-primary py-3 px-5 mt-3 wow zoomIn" data-wow-delay="0.9s">Solicitar cotización</a>
+                <a href="" class="btn btn-primary py-3 px-5 mt-3 wow zoomIn" data-wow-delay="0.9s">Brochure</a>
+
+            </div>
+            <div class="col-lg-5" style="min-height: 500px;">
+                <div class="position-relative h-100">
+                    <img class="position-absolute w-100 h-100 rounded wow zoomIn" data-wow-delay="0.9s" src="vistas/img/about.jpg" style="object-fit: cover;">
                 </div>
             </div>
         </div>
     </div>
-    <!-- Navbar End -->
+</div>
+<!-- Información End -->
 
 
-    <!-- Full Screen Search Start -->
-    <div class="modal fade" id="searchModal" tabindex="-1">
-        <div class="modal-dialog modal-fullscreen">
-            <div class="modal-content" style="background: rgba(9, 30, 62, .7);">
-                <div class="modal-header border-0">
-                    <button type="button" class="btn bg-white btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body d-flex align-items-center justify-content-center">
-                    <div class="input-group" style="max-width: 600px;">
-                        <input type="text" class="form-control bg-transparent border-primary p-3" placeholder="Type search keyword">
-                        <button class="btn btn-primary px-4"><i class="bi bi-search"></i></button>
+<!-- Service Start -->
+<div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
+    <div class="container py-5">
+        <div class="section-title text-center position-relative pb-3 mb-5 mx-auto" style="max-width: 600px;">
+            <h5 class="fw-bold text-primary text-uppercase">Nuestros Servicios</h5>
+            <h1 class="mb-0">Desarrollo de Software</h1>
+        </div>
+        <div class="row g-5">
+            <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.3s">
+                <div class="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
+                    <div class="service-icon">
+                        <i class="fa fa-shield-alt text-white"></i>
                     </div>
+                    <h4 class="mb-3">Sistema de Ventas</h4>
+                    <p class="m-0">Sistema de ventas a medida, de acuerdo a las necesidades del cliente</p>
+                    
                 </div>
+            </div>
+            <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.6s">
+                <div class="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
+                    <div class="service-icon">
+                        <i class="fa fa-chart-pie text-white"></i>
+                    </div>
+                    <h4 class="mb-3">Softwares Contables</h4>
+                    <p class="m-0">Automatiza las tareas contables de tu empresa.</p>
+                    
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.9s">
+                <div class="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
+                    <div class="service-icon">
+                        <i class="fa fa-code text-white"></i>
+                    </div>
+                    <h4 class="mb-3">Control de Asistencias</h4>
+                    <p class="m-0">Registra y gestiona la asistencia de tu perosonal.</p>
+                    
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="1.2s">
+                <div class="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
+                    <div class="service-icon">
+                        <i class="fa fa-code text-white"></i>
+                    </div>
+                    <h4 class="mb-3">Software Estadístico</h4>
+                    <p class="m-0">Análisis, interpretación y visualización de datos para una mejor toma de decisiones</p>
+                    
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="1.5s">
+                <div class="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
+                    <div class="service-icon">
+                        <i class="fa fa-code text-white"></i>
+                    </div>
+                    <h4 class="mb-3">Software de Ingeniería</h4>
+                    <p class="m-0">Análisis, simulación y optimización de proyectos y sistemas.</p>
+                    
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="1.8s">
+                <div class="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
+                    <div class="service-icon">
+                        <i class="fa fa-code text-white"></i>
+                    </div>
+                    <h4 class="mb-3">Software de Minería</h4>
+                    <p class="m-0">Gestion y control diferentes aspectos de la actividad minera, como la planificación de operaciones, la supervisión de equipos y la gestión de recursos.</p>
+                    
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="2.1s">
+                <a href="https://api.whatsapp.com/send?phone=997226188&text=Buen%20d%C3%ADa,%20quiero%20m%C3%A1s%20informaci%C3%B3n" target="_blank">
+                    <div class=" position-relative bg-primary rounded h-100 d-flex flex-column align-items-center justify-content-center text-center p-5">
+                        <h4 class="text-white mb-3">Contáctate con nosotros</h3>
+                            <p class="text-white mb-3">Escríbenos sin ningún compromiso al WhatsApp o por nuestras redes sociales</p>
+                            <h5 class="text-white mb-0">+51 997 226 188</h2>
+                    </div>
+                </a>
             </div>
         </div>
     </div>
-    <!-- Full Screen Search End -->
-
-
-    <!-- Service Start -->
-    <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
-        <div class="container py-5">
-            <div class="section-title text-center position-relative pb-3 mb-5 mx-auto" style="max-width: 600px;">
-                <h5 class="fw-bold text-primary text-uppercase">Nuestros Servicios</h5>
-                <h1 class="mb-0">Desarrollo de Software</h1>
-            </div>
-            <div class="row g-5">
-                <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.3s">
-                    <div class="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
-                        <div class="service-icon">
-                            <i class="fa fa-shield-alt text-white"></i>
-                        </div>
-                        <h4 class="mb-3">Sistema de Ventas</h4>
-                        <p class="m-0">Sistema de ventas a medida, de acuerdo a las necesidades del cliente</p>
-                        <a class="btn btn-lg btn-primary rounded" href="">
-                            <i class="bi bi-arrow-right"></i>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.6s">
-                    <div class="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
-                        <div class="service-icon">
-                            <i class="fa fa-chart-pie text-white"></i>
-                        </div>
-                        <h4 class="mb-3">Sitio Web</h4>
-                        <p class="m-0">Hazte conocido por el mundo, creamos tu sito web para presentarte a todo el mundo</p>
-                        <a class="btn btn-lg btn-primary rounded" href="">
-                            <i class="bi bi-arrow-right"></i>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.9s">
-                    <div class="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
-                        <div class="service-icon">
-                            <i class="fa fa-code text-white"></i>
-                        </div>
-                        <h4 class="mb-3">Desarrollo Web</h4>
-                        <p class="m-0">Creamos sistemas web multiplataforma para que puedas acceder a tus datos desde cualquier parte del palneta</p>
-                        <a class="btn btn-lg btn-primary rounded" href="">
-                            <i class="bi bi-arrow-right"></i>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.3s">
-                    <div class="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
-                        <div class="service-icon">
-                            <i class="fab fa-android text-white"></i>
-                        </div>
-                        <h4 class="mb-3">Dominio y Hosting</h4>
-                        <p class="m-0">Adquiere tu dominio y hosting y saca el máximo provecho a la virtualidad, tenemos los mejores palnes de acuerdo al cliente</p>
-                        <a class="btn btn-lg btn-primary rounded" href="">
-                            <i class="bi bi-arrow-right"></i>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.6s">
-                    <div class="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
-                        <div class="service-icon">
-                            <i class="fa fa-search text-white"></i>
-                        </div>
-                        <h4 class="mb-3">Testing Profesional</h4>
-                        <p class="m-0">¿Tienes un proyecto en desarrollo? Te asesoramos y te apoyamos en los procesos de desarrollo, testing y pruebas.</p>
-                        <a class="btn btn-lg btn-primary rounded" href="">
-                            <i class="bi bi-arrow-right"></i>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.9s">
-                    <div class="position-relative bg-primary rounded h-100 d-flex flex-column align-items-center justify-content-center text-center p-5">
-                        <h3 class="text-white mb-3">Contáctate con nosotros</h3>
-                        <p class="text-white mb-3">Escríbenos sin ningún compromiso al WhatsApp o por nuestras redes sociales</p>
-                        <h2 class="text-white mb-0">+51 997 226 188</h2>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Service End -->
-
-
-    
-    
+</div>
+<!-- Service End -->
