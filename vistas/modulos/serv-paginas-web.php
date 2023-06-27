236f28784aeede1f0d7d1f852b00f2bdf8b3b308
Jhosmer <jguevaram0897@gmail.com>
2023-06-27 10:52:25 -0500

    paginas-web

--- vistas/modulos/serv-paginas-web.php
+++ vistas/modulos/serv-paginas-web.php
@@ -1,157 +1,142 @@
 <div class="container-fluid bg-primary py-5 bg-header-paginas-web" style="margin-bottom: 90px;">
-            <div class="row py-5">
-                <div class="col-12 pt-lg-5 mt-lg-5 text-center">
-                    <h1 class="display-1 text-white animated zoomIn">Desarrollo de Páginas Web</h1>
+    <div class="row py-5">
+        <div class="col-12 pt-lg-5 mt-lg-5 text-center">
+            <h1 class="display-1 text-white animated zoomIn">Desarrollo de Páginas Web</h1>
+        </div>
+    </div>
+</div>
+</div>
+<!-- Navbar End -->
+
+
+<!-- Full Screen Search Start -->
+<div class="modal fade" id="searchModal" tabindex="-1">
+    <div class="modal-dialog modal-fullscreen">
+        <div class="modal-content" style="background: rgba(9, 30, 62, .7);">
+            <div class="modal-header border-0">
+                <button type="button" class="btn bg-white btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body d-flex align-items-center justify-content-center">
+                <div class="input-group" style="max-width: 600px;">
+                    <input type="text" class="form-control bg-transparent border-primary p-3" placeholder="Type search keyword">
+                    <button class="btn btn-primary px-4"><i class="bi bi-search"></i></button>
                 </div>
             </div>
         </div>
     </div>
-    <!-- Navbar End -->
+</div>
+<!-- Full Screen Search End -->
 
-
-    <!-- Full Screen Search Start -->
-    <div class="modal fade" id="searchModal" tabindex="-1">
-        <div class="modal-dialog modal-fullscreen">
-            <div class="modal-content" style="background: rgba(9, 30, 62, .7);">
-                <div class="modal-header border-0">
-                    <button type="button" class="btn bg-white btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+<!-- informacion sobre el desarrollo de paginas web -->
+<div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
+    <div class="container py-5">
+        <div class="row g-5">
+            <div class="col-lg-7">
+                <div class="section-title position-relative pb-3 mb-5">
+                    <h1 class="mb-0">Diseño y desarrollo de Páginas Web a medida y acorde con los requerimientos de tu negicio o empresa</h1>
                 </div>
-                <div class="modal-body d-flex align-items-center justify-content-center">
-                    <div class="input-group" style="max-width: 600px;">
-                        <input type="text" class="form-control bg-transparent border-primary p-3" placeholder="Type search keyword">
-                        <button class="btn btn-primary px-4"><i class="bi bi-search"></i></button>
+                <p class="mb-4">Te ayudamos con el diseño y creacion de tu propia página web. Repotencia tu negocio o empresa llegando a más personas y multiplica tus ventas</p>
+                <p class="mb-4">Nuestro servicio personalizado esta pensando en las necesidades de cada uno de nuestros clientes para ofrecerles las mejores soluciones en el desarrollo de paginas web, maximizando la visibilidad en línea de nuestros clientes y diferenciándose de la competitividad.</p>
+                <div class="row g-0 mb-3">
+                    <div class="col-sm-6 wow zoomIn" data-wow-delay="0.2s">
+                        <h6 class="mb-3"><i class="fa fa-check text-primary me-3"></i>Conecta a más clientes.</h6>
+                        <h6 class="mb-3"><i class="fa fa-check text-primary me-3"></i>Refuerza la imagen de tu empresa o negocio.</h6>
+                        <h6 class="mb-3"><i class="fa fa-check text-primary me-3"></i>Muestra tus productos o servicios las 24h.</h6>
                     </div>
+                    <div class="col-sm-6 wow zoomIn" data-wow-delay="0.4s">
+                        <h6 class="mb-3"><i class="fa fa-check text-primary me-3"></i>Incrementa tus ventas</h6>
+                        <h6 class="mb-3"><i class="fa fa-check text-primary me-3"></i>Mejor Posicinamiento en linea</h6>
+                        <h6 class="mb-3"><i class="fa fa-check text-primary me-3"></i>Crea una experiencia digital atractiva</h6>
+                    </div>
+                </div>
+                <div class="d-flex align-items-center mb-4 wow fadeIn" data-wow-delay="0.6s">
+                    <div class="bg-primary d-flex align-items-center justify-content-center rounded" style="width: 60px; height: 60px;">
+                        <i class="fa fa-phone-alt text-white"></i>
+                    </div>
+                    <div class="ps-4">
+                        <h6 class="mb-2">Para dudas y consultas, comunicarse al número</h6>
+                        <h6 class="text-primary mb-0">+51 997 226 188</h6>
+                    </div>
+                </div>
+                <a href="contacto" class="btn btn-primary py-3 px-5 mt-3 wow zoomIn" data-wow-delay="0.4s">Solicitar cotización</a>
+            </div>
+            <div class="col-lg-5" style="min-height: 500px;">
+                <div class="position-relative h-100">
+                    <img class="position-absolute w-100 h-100 rounded wow zoomIn" data-wow-delay="0.4s" src="vistas/img/imagenes/img-pagina-web-3.png">
                 </div>
             </div>
         </div>
     </div>
-    <!-- Full Screen Search End -->
+</div>
+<!-- About End -->
 
- <!-- informacion sobre el desarrollo de paginas web -->
- <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
-        <div class="container py-5">
-            <div class="row g-5">
-                <div class="col-lg-7">
-                    <div class="section-title position-relative pb-3 mb-5">
-                        <h1 class="mb-0">Diseño y desarrollo de Páginas Web a medida y acorde con los requerimientos de tu negicio o empresa</h1>
+
+
+
+<!-- Service Start -->
+<div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
+    <div class="container py-5">
+        <div class="section-title text-center position-relative pb-3 mb-5 mx-auto" style="max-width: 600px;">
+            <h5 class="fw-bold text-primary text-uppercase">Más de nuestros servicios</h5>
+            <h1 class="mb-0">Conoce más de nuestras Páginas Web</h1>
+        </div>
+        <div class="row g-5">
+            <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.3s">
+                <div class="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
+                    <div class="service-icon">
+                        <i class="fa fa-shield-alt text-white"></i>
                     </div>
-                    <p class="mb-4">Te ayudamos con el diseño y creacion de tu propia página web. Repotencia tu negocio o empresa llegando a más personas y multiplica tus ventas</p>
-                    <p class="mb-4">Nuestro servicio personalizado esta pensando en las necesidades de cada uno de nuestros clientes para ofrecerles las mejores soluciones en el desarrollo de paginas web, maximizando la visibilidad en línea de nuestros clientes y diferenciándose de la competitividad.</p>
-                    <div class="row g-0 mb-3">
-                        <div class="col-sm-6 wow zoomIn" data-wow-delay="0.2s">
-                            <h5 class="mb-3"><i class="fa fa-check text-primary me-3"></i>Orientación al cliente</h5>
-                            <h5 class="mb-3"><i class="fa fa-check text-primary me-3"></i>Innovación constante</h5>
-                            <h5 class="mb-3"><i class="fa fa-check text-primary me-3"></i>Competitividad</h5>
-                            <h5 class="mb-3"><i class="fa fa-check text-primary me-3"></i>Equipo de profesionales</h5>
-                        </div>
-                        <div class="col-sm-6 wow zoomIn" data-wow-delay="0.4s">
-                            <h5 class="mb-3"><i class="fa fa-check text-primary me-3"></i>Seguridad</h5>
-                            <h5 class="mb-3"><i class="fa fa-check text-primary me-3"></i>Compromiso</h5>
-                            <h5 class="mb-3"><i class="fa fa-check text-primary me-3"></i>Soporte 24/7</h5>
-                            <h5 class="mb-3"><i class="fa fa-check text-primary me-3"></i>Precios accesibles</h5>
-                        </div>
-                    </div>
-                    <div class="d-flex align-items-center mb-4 wow fadeIn" data-wow-delay="0.6s">
-                        <div class="bg-primary d-flex align-items-center justify-content-center rounded" style="width: 60px; height: 60px;">
-                            <i class="fa fa-phone-alt text-white"></i>
-                        </div>
-                        <div class="ps-4">
-                            <h5 class="mb-2">Para dudas y consultas, comunicarse al número</h5>
-                            <h4 class="text-primary mb-0">+51 997 226 188</h4>
-                        </div>
-                    </div>
-                    <a href="contacto" class="btn btn-primary py-3 px-5 mt-3 wow zoomIn" data-wow-delay="0.9s">Solicitar cotización</a>
+                    <h4 class="mb-3">Tiendas Virtuales</h4>
+                    <p class="m-0">Proporciona una plataforma fácil de usar y seleccionar tus productos, realizar pagos y transacciones seguras. Amplía tu audiencia y clientes al realizar compras cómodamente desde cualquier lugar y momento.</p>
+                    <!-- <a class="btn btn-lg btn-primary rounded" href="">
+                            <i class="bi bi-arrow-right"></i>
+                        </a> -->
                 </div>
-                <div class="col-lg-5" style="min-height: 500px;">
-                    <div class="position-relative h-100">
-                        <img class="position-absolute w-100 h-100 rounded wow zoomIn" data-wow-delay="0.9s" src="vistas/img/about.jpg" style="object-fit: cover;">
+            </div>
+            <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.6s">
+                <div class="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
+                    <div class="service-icon">
+                        <i class="fa fa-chart-pie text-white"></i>
                     </div>
+                    <h4 class="mb-3">Hoteles</h4>
+                    <p class="m-0">"Crea tu página web personalizada y adaptaba a las nesecidades de tu negocio. Refleja la imagen y los valores de tu hotel a traves de su pripia página web"</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.9s">
+                <div class="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
+                    <div class="service-icon">
+                        <i class="fa fa-code text-white"></i>
+                    </div>
+                    <h4 class="mb-3">Restaurantes</h4>
+                    <p class="m-0">Te brindamos la posibilidad de integrar herramientas digitales para tu restaurante y a su vez que sea de facil uso tanto para ti como para tus clientes</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.3s">
+                <div class="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
+                    <div class="service-icon">
+                        <i class="fab fa-android text-white"></i>
+                    </div>
+                    <h4 class="mb-3">Instituciones Educativas</h4>
+                    <p class="m-0">Abre las puerta al conocimiento y crea oportunidades para el futuro mostrando tu pagina web tanto a padres de familia y estudiantes </p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.6s">
+                <div class="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
+                    <div class="service-icon">
+                        <i class="fa fa-search text-white"></i>
+                    </div>
+                    <h4 class="mb-3">Gestión de inventarios</h4>
+                    <p class="m-0">Garantiza la disposición adecuada de productos en el momento adecuado, evitando la escasez o el exceso de existencias, y maximizando así la eficiencia operativa de tu empresa y la satisfacción del cliente.</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.9s">
+                <div class="position-relative bg-primary rounded h-100 d-flex flex-column align-items-center justify-content-center text-center p-5">
+                    <h3 class="text-white mb-3">Contáctate con nosotros</h3>
+                    <p class="text-white mb-3">Escríbenos sin ningún compromiso al WhatsApp o por nuestras redes sociales</p>
+                    <h2 class="text-white mb-0">+51 997 226 188</h2>
                 </div>
             </div>
         </div>
     </div>
-    <!-- About End -->
-
-
-
-
-    <!-- Service Start -->
-    <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
-        <div class="container py-5">
-            <div class="section-title text-center position-relative pb-3 mb-5 mx-auto" style="max-width: 600px;">
-                <h5 class="fw-bold text-primary text-uppercase">Nuestros Servicios</h5>
-                <h1 class="mb-0">Desarrollo de Páginas Web</h1>
-            </div>
-            <div class="row g-5">
-                <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.3s">
-                    <div class="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
-                        <div class="service-icon">
-                            <i class="fa fa-shield-alt text-white"></i>
-                        </div>
-                        <h4 class="mb-3">Tienda virtual</h4>
-                        <p class="m-0">Tu tienda virtual desde tu dispositvo móvil, vende desde cualquier lado.</p>
-                        <a class="btn btn-lg btn-primary rounded" href="">
-                            <i class="bi bi-arrow-right"></i>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.6s">
-                    <div class="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
-                        <div class="service-icon">
-                            <i class="fa fa-chart-pie text-white"></i>
-                        </div>
-                        <h4 class="mb-3">Educación</h4>
-                        <p class="m-0">Creamos aplicativos para educación.</p>
-                        <a class="btn btn-lg btn-primary rounded" href="">
-                            <i class="bi bi-arrow-right"></i>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.9s">
-                    <div class="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
-                        <div class="service-icon">
-                            <i class="fa fa-code text-white"></i>
-                        </div>
-                        <h4 class="mb-3">Videojuegos</h4>
-                        <p class="m-0">¿Tienes un juego en mente? Hazlo realidad con nuestro equipo profesional de desarrollo.</p>
-                        <a class="btn btn-lg btn-primary rounded" href="">
-                            <i class="bi bi-arrow-right"></i>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.3s">
-                    <div class="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
-                        <div class="service-icon">
-                            <i class="fab fa-android text-white"></i>
-                        </div>
-                        <h4 class="mb-3">Play Store</h4>
-                        <p class="m-0">Te asesoramos para que puedas subir tus aplicativos a Play Store para android.</p>
-                        <a class="btn btn-lg btn-primary rounded" href="">
-                            <i class="bi bi-arrow-right"></i>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.6s">
-                    <div class="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
-                        <div class="service-icon">
-                            <i class="fa fa-search text-white"></i>
-                        </div>
-                        <h4 class="mb-3">App Store</h4>
-                        <p class="m-0">Te asesoramos para que subas tus aplicativos a la App Store de Apple.</p>
-                        <a class="btn btn-lg btn-primary rounded" href="">
-                            <i class="bi bi-arrow-right"></i>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.9s">
-                    <div class="position-relative bg-primary rounded h-100 d-flex flex-column align-items-center justify-content-center text-center p-5">
-                        <h3 class="text-white mb-3">Contáctate con nosotros</h3>
-                        <p class="text-white mb-3">Escríbenos sin ningún compromiso al WhatsApp o por nuestras redes sociales</p>
-                        <h2 class="text-white mb-0">+51 997 226 188</h2>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Service End -->
-    
+</div>
+<!-- Service End -->
