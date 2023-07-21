21511f29b30e41b195baeeab534865bab1b1ffca
Jhosmer <jguevaram0897@gmail.com>
2023-07-21 12:26:58 -0500

    actualizacion

--- vistas/modulos/geomekh.php
+++ vistas/modulos/geomekh.php
@@ -12,25 +12,25 @@
 <div class="container-fluid wow fadeInUp" data-wow-delay="0.1s">
     <div class="container">
         <div class="owl-carousel testimonial-carousel wow fadeInUp" data-wow-delay="0.6s">
-            <div class="card testimonial-item bg-light my-4">
+            <div class="card testimonial-item bg-light mx-2 p-1 border-2 border-dark rounded card-small">
                 <img class="img-fluid rounded" src="vistas/img/geomekh/geomekh-1.jpg" alt="">
             </div>
-            <div class="testimonial-item bg-light my-4">
+            <div class="card testimonial-item bg-light mx-2 p-1 border-2 border-dark rounded card-small">
                 <img class="img-fluid rounded" src="vistas/img/geomekh/geomekh-2.jpg" alt="">
             </div>
-            <div class="testimonial-item bg-light my-4">
+            <div class="card testimonial-item bg-light mx-2 p-1 border-2 border-dark rounded card-small">
                 <img class="img-fluid rounded" src="vistas/img/geomekh/geomekh-3.jpg" alt="">
             </div>
-            <div class="testimonial-item bg-light my-4">
+            <div class="card testimonial-item bg-light mx-2 p-1 border-2 border-dark rounded card-small">
                 <img class="img-fluid rounded" src="vistas/img/geomekh/geomekh-4.jpg" alt="">
             </div>
-            <div class="testimonial-item bg-light my-4">
+            <div class="card testimonial-item bg-light mx-2 p-1 border-2 border-dark rounded card-small">
                 <img class="img-fluid rounded" src="vistas/img/geomekh/geomekh-5.jpg" alt="">
             </div>
-            <div class="testimonial-item bg-light my-4">
+            <div class="card testimonial-item bg-light mx-2 p-1 border-2 border-dark rounded card-small">
                 <img class="img-fluid rounded" src="vistas/img/geomekh/geomekh-6.jpg" alt="">
             </div>
-            <div class="testimonial-item bg-light my-4">
+            <div class="card testimonial-item bg-light mx-2 p-1 border-2 border-dark rounded card-small">
                 <img class="img-fluid rounded" src="vistas/img/geomekh/geomekh-7.jpg" alt="">
             </div>
         </div>
@@ -48,10 +48,10 @@
                     <h1 class="mb-0">Optimiza el mapeo de taludes desde la palma de tu mano</h1>
                 </div>
                 <p class="mb-4">GEOMEKH busca optimizar la labor de mapeo geomecánico de frentes o celdas del macizo rocoso con el objetivo de tener una herramienta tecnológica que nos va a permitir hacer este estudio geomecánico de forma más dinámica e interactiva y en mucho menor tiempo del que se hace actualmente y así poder identificar un posible incidente geomecánico como los anteriores mencionados, pero en tiempo real e in situ para tomar decisiones y poder prevenirlos.</p>
-                <p class="mb-4"> Geomekh cuenta con tres módulos; el primero “Parámetros” aquí podemos ingresar los datos de forma manual según las teorías de RMR de Bienawski, Q de Barton o GSI de forma que, al culminar el ingreso de datos, con solo presionar un botón nos arrojará los resultados específicos para los datos tomados, generando una base de datos de toda la información que vamos registrando y calculando, además de un reporte en pdf o Excel que podemos exportar. Además, cuenta con ayudas teóricas y videotutoriales para poder tomar los datos de forma adecuada en campo. El segundo módulo “Estereografía” funciona ingresando datos de rumbo y buzamiento de todas las estructuras identificadas en la celda o área de estudio, de forma que el aplicativo puede procesar esta data y generar las respectivas proyecciones estereográficas (Diagrama de planos, Concentración de polos y Diagrama de Roseta) muy similar a como lo hacen los software Stereonet o Dips, con la diferencia de que todo esto se realiza dentro del app Geomekh, esto nos permite identificar los tipos de falla que se puede dar en el macizo rocoso en tiempo real y así poder evitar algún tipo de incidente geomecánico con su detección temprana. Por últimos estamos trabajando en el tercer módulo “Proyectos” el cual tiene las mismas funciones de los módulos anteriores, pero estos están incorporados en uno solo e incluyen más funciones y campos específicos para trabajar en proyectos que tenga cientos y miles de puntos o celdas de estudio como sucede en proyectos de geomecánica para minería y construcción de carreteras. De esta forma estamos llegando tanto a los estudiantes universitarios, profesionales y empresas del rubro.
+                <p class="mb-4"> Geomekh cuenta con tres módulos; el primero “Parámetros” aquí podrás ingresar los datos de forma manual según las teorías de RMR de Bienawski, Q de Barton o GSI de forma que, al culminar el ingreso de datos, arrojará los resultados específicos para los datos tomados, generando una base de datos de toda la información que vamos registrando y calculando, además de un reporte en pdf o Excel que podemos exportar. El segundo módulo “Estereografía” funciona ingresando datos de rumbo y buzamiento de todas las estructuras identificadas en la celda o área de estudio, de forma que el aplicativo puede procesar esta data y generar las respectivas proyecciones estereográficas (Diagrama de planos, Concentración de polos y Diagrama de Roseta) muy similar a como lo hacen los software Stereonet o Dips, con la diferencia de que todo esto se realiza dentro del app Geomekh, esto nos permite identificar los tipos de falla que se puede dar en el macizo rocoso en tiempo real y así poder evitar algún tipo de incidente geomecánico con su detección temprana. Por último, el módulo de “Proyectos” el cual tiene las mismas funciones de los módulos anteriores, pero estos están incorporados en uno solo e incluyen más funciones y campos específicos para trabajar en proyectos que tenga cientos y miles de puntos o celdas de estudio como sucede en proyectos de geomecánica para minería y construcción de carreteras. De esta forma estamos llegando tanto a los estudiantes universitarios, profesionales y empresas del rubro.
                 </p>
 
-                <div class="row g-0 mb-3">
+                <!-- <div class="row g-0 mb-3">
                     <div class="col-sm-6 wow zoomIn" data-wow-delay="0.2s">
                         <h5 class="mb-3"><i class="fa fa-check text-primary me-3"></i>Award Winning</h5>
                         <h5 class="mb-3"><i class="fa fa-check text-primary me-3"></i>Professional Staff</h5>
@@ -60,17 +60,17 @@
                         <h5 class="mb-3"><i class="fa fa-check text-primary me-3"></i>24/7 Support</h5>
                         <h5 class="mb-3"><i class="fa fa-check text-primary me-3"></i>Fair Prices</h5>
                     </div>
-                </div>
+                </div> -->
                 <div class="d-flex align-items-center mb-4 wow fadeIn" data-wow-delay="0.6s">
                     <div class="bg-primary d-flex align-items-center justify-content-center rounded" style="width: 60px; height: 60px;">
                         <i class="fa fa-phone-alt text-white"></i>
                     </div>
                     <div class="ps-4">
-                        <h5 class="mb-2">Call to ask any question</h5>
-                        <h4 class="text-primary mb-0">+012 345 6789</h4>
+                        <h6 class="mb-2">Para consultas:</h6>
+                        <h6 class="text-primary mb-0">+51 997 226 188</h6>
                     </div>
                 </div>
-                <a href="quote.html" class="btn btn-primary py-3 px-5 mt-3 wow zoomIn" data-wow-delay="0.9s">Request A Quote</a>
+
             </div>
             <div class="col-lg-5" style="min-height: 500px;">
                 <div class="position-relative h-100">
@@ -86,8 +86,8 @@
 <div class="container-fluid wow fadeInUp" data-wow-delay="0.1s">
     <div class="container py-5">
         <div class="section-title text-center position-relative pb-3 mb-5 mx-auto" style="max-width: 600px;">
-            <h5 class="fw-bold text-primary text-uppercase">Módulos</h5>
-            <h1 class="mb-0">Módulos que facilitan tu trabajo y gestion de mantemieniento</h1>
+            <h5 class="fw-bold text-primary text-uppercase"></h5>
+            <h1 class="mb-0">Módulos Geomekh</h1>
         </div>
         <div class="row g-5">
             <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.3s">
@@ -95,8 +95,8 @@
                     <div class="service-icon">
                         <i class="fa fa-shield-alt text-white"></i>
                     </div>
-                    <h4 class="mb-3">Cyber Security</h4>
-                    <p class="m-0">Amet justo dolor lorem kasd amet magna sea stet eos vero lorem ipsum dolore sed</p>
+                    <h4 class="mb-3">Vista principal</h4>
+                    <p class="m-0">Muestra algunas características del aplicativo y ayudas informativas de cómo debería llenarse de forma adecuada cada campo de los parámetros RMR, Q de Barton y GSI según sea el caso</p>
                     <a class="btn btn-lg btn-primary rounded" href="">
                         <i class="bi bi-arrow-right"></i>
                     </a>
@@ -107,8 +107,8 @@
                     <div class="service-icon">
                         <i class="fa fa-chart-pie text-white"></i>
                     </div>
-                    <h4 class="mb-3">Data Analytics</h4>
-                    <p class="m-0">Amet justo dolor lorem kasd amet magna sea stet eos vero lorem ipsum dolore sed</p>
+                    <h4 class="mb-3">Vista de Módulo RMR</h4>
+                    <p class="m-0">Registra las características geomecánicas de la roca según los parámetros RMR de Bieniawki manualmente. Adicionalmente cuenta con ayudas para un llenado más rápido</p>
                     <a class="btn btn-lg btn-primary rounded" href="">
                         <i class="bi bi-arrow-right"></i>
                     </a>
@@ -119,8 +119,8 @@
                     <div class="service-icon">
                         <i class="fa fa-code text-white"></i>
                     </div>
-                    <h4 class="mb-3">Web Development</h4>
-                    <p class="m-0">Amet justo dolor lorem kasd amet magna sea stet eos vero lorem ipsum dolore sed</p>
+                    <h4 class="mb-3">Vista de Orientación</h4>
+                    <p class="m-0">Permite ingresar valores de Rumbo y Buzamiento del frente de estudio con el objetivo de obtener proyecciones estereográficas (Diagrama de Planos, Concentración de polos y Diagrama de Roseta)</p>
                     <a class="btn btn-lg btn-primary rounded" href="">
                         <i class="bi bi-arrow-right"></i>
                     </a>
--- vistas/modulos/logtekh.php
+++ vistas/modulos/logtekh.php
@@ -102,7 +102,7 @@
                         <i class="fa fa-chart-pie text-white"></i>
                     </div>
                     <h4 class="mb-3">Formulario de registro Logueo</h4>
-                    <p class="m-0">Contiene:</p>
+                    <p class="m-0">Contiene: campos para registro según parámetros de RMR de Bienaswki o Q de Barton, ademas de contar con ayudas visuales. </p>
                     <a class="btn btn-lg btn-primary rounded" href="">
                         <i class="bi bi-arrow-right"></i>
                     </a>
@@ -113,8 +113,8 @@
                     <div class="service-icon">
                         <i class="fa fa-code text-white"></i>
                     </div>
-                    <h4 class="mb-3">Web Development</h4>
-                    <p class="m-0">Amet justo dolor lorem kasd amet magna sea stet eos vero lorem ipsum dolore sed</p>
+                    <h4 class="mb-3"> Reportes </h4>
+                    <p class="m-0">Contiene: filtros (sondajes, tramos de fechas, tramos de sondaje) y data resumida del logueo para generar un reporte detallado y luego exportat a Excel.</p>
                     <a class="btn btn-lg btn-primary rounded" href="">
                         <i class="bi bi-arrow-right"></i>
                     </a>
@@ -125,105 +125,16 @@
                     <div class="service-icon">
                         <i class="fab fa-android text-white"></i>
                     </div>
-                    <h4 class="mb-3">Apps Development</h4>
-                    <p class="m-0">Amet justo dolor lorem kasd amet magna sea stet eos vero lorem ipsum dolore sed</p>
+                    <h4 class="mb-3">Módulo de Estereografía</h4>
+                    <p class="m-0">Selecciona el sondaje de tu interés y genera proyecciones estereogáficas(Diagramas de planos, concentración de polos y diagrama de Rosetas), todo esto dentro de nuestro mismo software</p>
                     <a class="btn btn-lg btn-primary rounded" href="">
                         <i class="bi bi-arrow-right"></i>
                     </a>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.6s">
-                <div class="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
-                    <div class="service-icon">
-                        <i class="fa fa-search text-white"></i>
-                    </div>
-                    <h4 class="mb-3">SEO Optimization</h4>
-                    <p class="m-0">Amet justo dolor lorem kasd amet magna sea stet eos vero lorem ipsum dolore sed</p>
-                    <a class="btn btn-lg btn-primary rounded" href="">
-                        <i class="bi bi-arrow-right"></i>
-                    </a>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.6s">
-                <div class="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
-                    <div class="service-icon">
-                        <i class="fa fa-search text-white"></i>
-                    </div>
-                    <h4 class="mb-3">SEO Optimization</h4>
-                    <p class="m-0">Amet justo dolor lorem kasd amet magna sea stet eos vero lorem ipsum dolore sed</p>
-                    <a class="btn btn-lg btn-primary rounded" href="">
-                        <i class="bi bi-arrow-right"></i>
-                    </a>
-                </div>
-            </div>
+        
         </div>
     </div>
 </div>
 <!-- Módulos End -->
 
-<!-- Planes Start -->
-<div class="container-fluid wow fadeInUp" data-wow-delay="0.1s">
-    <div class="container py-5">
-        <div class="section-title text-center position-relative pb-3 mb-5 mx-auto" style="max-width: 600px;">
-            <h5 class="fw-bold text-primary text-uppercase">Pricing Plans</h5>
-            <h1 class="mb-0">We are Offering Competitive Prices for Our Clients</h1>
-        </div>
-        <div class="row g-0">
-            <div class="col-lg-4 wow slideInUp" data-wow-delay="0.6s">
-                <div class="bg-light rounded">
-                    <div class="border-bottom py-4 px-5 mb-4">
-                        <h4 class="text-primary mb-1">Basic Plan</h4>
-                        <small class="text-uppercase">For Small Size Business</small>
-                    </div>
-                    <div class="p-5 pt-0">
-                        <h1 class="display-5 mb-3">
-                            <small class="align-top" style="font-size: 22px; line-height: 45px;">$</small>49.00<small class="align-bottom" style="font-size: 16px; line-height: 40px;">/ Month</small>
-                        </h1>
-                        <div class="d-flex justify-content-between mb-3"><span>HTML5 & CSS3</span><i class="fa fa-check text-primary pt-1"></i></div>
-                        <div class="d-flex justify-content-between mb-3"><span>Bootstrap v5</span><i class="fa fa-check text-primary pt-1"></i></div>
-                        <div class="d-flex justify-content-between mb-3"><span>Responsive Layout</span><i class="fa fa-times text-danger pt-1"></i></div>
-                        <div class="d-flex justify-content-between mb-2"><span>Cross-browser Support</span><i class="fa fa-times text-danger pt-1"></i></div>
-                        <a href="" class="btn btn-primary py-2 px-4 mt-4">Order Now</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 wow slideInUp" data-wow-delay="0.3s">
-                <div class="bg-white rounded shadow position-relative" style="z-index: 1;">
-                    <div class="border-bottom py-4 px-5 mb-4">
-                        <h4 class="text-primary mb-1">Standard Plan</h4>
-                        <small class="text-uppercase">For Medium Size Business</small>
-                    </div>
-                    <div class="p-5 pt-0">
-                        <h1 class="display-5 mb-3">
-                            <small class="align-top" style="font-size: 22px; line-height: 45px;">$</small>99.00<small class="align-bottom" style="font-size: 16px; line-height: 40px;">/ Month</small>
-                        </h1>
-                        <div class="d-flex justify-content-between mb-3"><span>HTML5 & CSS3</span><i class="fa fa-check text-primary pt-1"></i></div>
-                        <div class="d-flex justify-content-between mb-3"><span>Bootstrap v5</span><i class="fa fa-check text-primary pt-1"></i></div>
-                        <div class="d-flex justify-content-between mb-3"><span>Responsive Layout</span><i class="fa fa-check text-primary pt-1"></i></div>
-                        <div class="d-flex justify-content-between mb-2"><span>Cross-browser Support</span><i class="fa fa-times text-danger pt-1"></i></div>
-                        <a href="" class="btn btn-primary py-2 px-4 mt-4">Order Now</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 wow slideInUp" data-wow-delay="0.9s">
-                <div class="bg-light rounded">
-                    <div class="border-bottom py-4 px-5 mb-4">
-                        <h4 class="text-primary mb-1">Advanced Plan</h4>
-                        <small class="text-uppercase">For Large Size Business</small>
-                    </div>
-                    <div class="p-5 pt-0">
-                        <h1 class="display-5 mb-3">
-                            <small class="align-top" style="font-size: 22px; line-height: 45px;">$</small>149.00<small class="align-bottom" style="font-size: 16px; line-height: 40px;">/ Month</small>
-                        </h1>
-                        <div class="d-flex justify-content-between mb-3"><span>HTML5 & CSS3</span><i class="fa fa-check text-primary pt-1"></i></div>
-                        <div class="d-flex justify-content-between mb-3"><span>Bootstrap v5</span><i class="fa fa-check text-primary pt-1"></i></div>
-                        <div class="d-flex justify-content-between mb-3"><span>Responsive Layout</span><i class="fa fa-check text-primary pt-1"></i></div>
-                        <div class="d-flex justify-content-between mb-2"><span>Cross-browser Support</span><i class="fa fa-check text-primary pt-1"></i></div>
-                        <a href="" class="btn btn-primary py-2 px-4 mt-4">Order Now</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- Planes End -->
